4d5c99d9ca73fadc0fe74de157b12f772aa0363c
VegadelaLyra <lyradeperseo@gmail.com>
2023-02-23 19:02:11 -0500

    Email module

--- modules/04-email.js
+++ modules/04-email.js
@@ -2,18 +2,23 @@ import { launch } from "puppeteer"
 
 await getRandomEmail()
 export async function getRandomEmail() {
-    try {
-        // Start headless pupeteer browser on URL
-        const URL = 'https://temp-mail.org/en/'
-        const browser = await launch({headless: false})
-        const page = await browser.pages()
-        await page[0].goto(URL)
-        
-        // Search for random email on temp-mail.org website
-        // await page[0].waitForRequest('https://web2.temp-mail.org/messages')
-        const mail = await page[0].$eval('#mail', element => element.textContent)
-        console.log(mail)
-        await browser.close()
-        return mail
-    } catch (err) { console.log(err) }
+    // Start headless pupeteer browser on URL
+    const URL = 'https://temp-mail.org/en/'
+    const browser = await launch({headless: false})
+    const page = await browser.pages()
+    await page[0].goto(URL, {waitUntil: 'domcontentloaded'})
+    
+    // Search for random email on temp-mail.org website
+    // await page[0].waitForRequest('https://web2.temp-mail.org/messages')
+    await page[0].setRequestInterception(true)
+    page[0].on('request', async req => {
+        const URL = 'https://web2.temp-mail.org/messages'
+        if (req.url() !== URL ) return req.abort()
+        page[0].removeAllListeners()
+        return req.continue()
+    })
+    const mail = await page[0].$eval('#mail', element => element.textContent)
+    console.log(mail)
+    // await browser.close()
+    return mail
 }
