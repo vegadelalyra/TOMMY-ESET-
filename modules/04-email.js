8dd65bafe73715435bb786a70ad1bbaedbc24bac
VegadelaLyra <lyradeperseo@gmail.com>
2023-03-10 22:39:31 -0500

    TOMMY EESET: Now we can get random emails from internet in 30s.

--- modules/04-email.js
+++ modules/04-email.js
@@ -1,24 +1,27 @@
 import { launch } from "puppeteer"
+import { execSync } from 'child_process'
 
 await getRandomEmail()
 export async function getRandomEmail() {
     // Start headless pupeteer browser on URL
     const URL = 'https://temp-mail.org/en/'
-    const browser = await launch({headless: false})
+    const browser = await launch({ headless: false, timeout: 0 })
     const page = await browser.pages()
-    await page[0].goto(URL, {waitUntil: 'domcontentloaded'})
     
+    // Runs puppeteer browser minimized
+    const session = await page[0].target().createCDPSession()
+    const {windowId} = await session.send('Browser.getWindowForTarget')
+    await session.send('Browser.setWindowBounds', 
+    {windowId, bounds: {windowState: 'minimized'}})
+    await page[0].goto(URL, {waitUntil: 'domcontentloaded'})
+
+
     // Search for random email on temp-mail.org website
-    // await page[0].waitForRequest('https://web2.temp-mail.org/messages')
-    await page[0].setRequestInterception(true)
-    page[0].on('request', async req => {
-        const URL = 'https://web2.temp-mail.org/messages'
-        if (req.url() !== URL ) return req.abort()
-        page[0].removeAllListeners()
-        return req.continue()
-    })
-    const mail = await page[0].$eval('#mail', element => element.textContent)
-    console.log(mail)
-    // await browser.close()
-    return mail
+    await page[0].waitForRequest('https://web2.temp-mail.org/messages')
+    await page[0].click("#click-to-copy")
+    await browser.close()
+
+    // Email copied, afterwards, extract it from clipboard and console log it
+    const mail = execSync('powershell.exe Get-Clipboard -Raw').toString().trim()
+    return console.log(mail)
 }
